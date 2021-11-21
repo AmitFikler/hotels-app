f8cb5e03acdcb777924bf85ae381535ff9fd73ee
AmitFikler <amitfikler@gmail.com>
2021-11-21 13:31:15 +0200

    The site communicates with the data

--- hotels-app/src/App.js
+++ hotels-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import HotelsGallery from './Components/HotelsGallery';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>Hotels in Haifa</h1>
+        <HotelsGallery />
+      </div>
+    );
   }
 }
 
