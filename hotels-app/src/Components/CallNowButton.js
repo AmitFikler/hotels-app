b29b2d4dbe2bb87c7f6ad3df398683b3ccfc0efa
AmitFikler <amitfikler@gmail.com>
2021-11-21 14:42:41 +0200

    add call now component

---
+++ hotels-app/src/Components/CallNowButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class CallNowButton extends React.Component {
+  render() {
+    return <button>Call Now!</button>;
+  }
+  //TODO add on click call
+}
+
+export default CallNowButton;
--- hotels-app/src/Components/HotelPage.js
+++ hotels-app/src/Components/HotelPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import CallNowButton from './CallNowButton';
 class HotelPage extends React.Component {
   render() {
     return (
       <div>
         {this.props.name}---{this.props.street}
+        <CallNowButton />
       </div>
     );
   }
